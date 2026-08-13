fb82b5c5a6fed03563c56a027a545252edf7e801
user <americoidat28@gmail.com>
2026-08-12 19:35:18 -0500

    Clase 3 de Platzi-Configuracion de entorno de desarrollo

---
+++ src/main.js
@@ -0,0 +1,1 @@
+console.log("API URL=https://api.themoviedb.org/3/movie/popular?api_key=" + API_KEY);
---
+++ src/secrets.example.js
@@ -0,0 +1,2 @@
+// Crea porfavor un archivo secrets.js con tu API KEY
+const API_KEY = 'tu_api_key_aqui'; // Reemplaza 'tu_api_key_aqui' con tu clave de API real
